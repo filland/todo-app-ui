a1119ebe855d6de19f93efbe7f6c07f0b3730d24
filland <kubalexei@gmail.com>
2020-02-21 00:47:13 +0300

    redeploy to heroku

--- src/service/constants.js
+++ src/service/constants.js
@@ -18,11 +18,9 @@ export const WEB_UI_ROOT_URL = UI_ROOT_URL;
 export const REGISTRATION_CONFIRMATION_URL = WEB_UI_ROOT_URL + "/registration-confirmation";
 export const OAUTH2_REDIRECT_URI = WEB_UI_ROOT_URL+"/oauth2/redirect";
 
-
 // application API URLs
 export const API_ROOT_URL = SERVER_ROOT_URL + "/api/v1";
 export const GITHUB_AUTH_URL = SERVER_ROOT_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
-
 
 // key for retreiving value for the Authorization header for basic authentication
 export const BASIC_AUTH_CREDS = "basicAuthCreds";
